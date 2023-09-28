38643a8ef57efee6395a1ba9134978e231b1b069
billybu10 <101750525+billybu10@users.noreply.github.com>
2023-09-28 11:29:02 +0000

    Add files via upload

---
+++ example.uwu.cpp
@@ -0,0 +1,26 @@
+#include "c_pwus_pwus.h"
+#include <iostream>
+
+using namespace std;
+
+cwass Nyaa{
+pwivate: 
+    stwing name;
+
+pubwic:
+    Nyaa(){}
+    void setName(stwing name){
+        this->name = name;
+    }
+
+    stwing getName(){
+        wetuwn name;
+    }
+};
+
+int main(){
+    Nyaa gattito = Nyaa();
+    gattito.setName("Neko");
+    cout<< gattito.getName()<<endl;
+    wetuwn 0;
+}
